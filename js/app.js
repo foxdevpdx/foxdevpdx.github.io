216788a95632cb4c96dfa9c2dcf72dddc6a50b52
Devon Fox <foxdev@pdx.edu>
2022-05-29 18:29:32 -0700

    updated index template, deleted other pages

--- js/app.js
+++ js/app.js
@@ -1,50 +1,42 @@
 $(document).ready(function () {
-
-
-  // Below is my API call for weather and I've commented it out, as it perhaps is not important
-  // const url =
-  //   "https://api.openweathermap.org/data/2.5/weather?lat=45.5152&lon=-122.6784&appid=0a8f59036af137c0dc466abd93781d1c&units=imperial";
-
-  // $("#main").append("<div>").attr("id", "okay");
-  // // $("#okay").append($("<p>").text('Portland, OR Weather:'));
-
-  // const weatherCall = (weather) => {
-  //   // let today = Date();
-  //   // console.log(today)
-  //   $("#okay")
-  //     .append(
-  //       $("<p>")
-  //         .css({
-  //           color: "black",
-  //           marginTop: '8em',
-  //         })
-  //         .text(weather.name + " Weather")
-  //     )
-  //     .append(
-  //       $("<p>")
-  //         .css({
-  //           color: "black",
-  //           fontSize: "1.25em",
-  //         })
-  //         .text(parseInt(weather.main.temp) + "° " + weather.weather[0].main)
-  //     )
-      
-  // };
-
-  // const fetchData = (url) => {
-  //   $.ajax({
-  //     type: "GET",
-  //     url: url,
-  //     success: (data) => {
-  //       console.log(data);
-  //       weatherCall(data);
-  //     },
-  //     error: (error) => {
-  //       console.log(error);
-  //       $("#main").append("<div>").text(`An error occurred. Please try again.`);
-  //     },
-  //     complete: () => {},
-  //   });
-  // };
-  // fetchData(url);
+  //Below is my API call for weather and I've commented it out, as it perhaps is not important
+  const url =
+    'https://api.openweathermap.org/data/2.5/weather?lat=45.5152&lon=-122.6784&appid=0a8f59036af137c0dc466abd93781d1c&units=imperial';
+  // $('#main').append('<div>').attr('id', 'weather');
+  // $("#okay").append($("<p>").text('Portland, OR Weather:'));
+  const weatherCall = (weather) => {
+    // let today = Date();
+    // console.log(today)
+    $('#weather').append(
+      $('<div>')
+        .css({
+          fontWeight: '100',
+          color: 'black',
+          fontSize: '1.25em',
+        })
+        .text(
+          weather.name +
+            ' ' +
+            parseInt(weather.main.temp) +
+            '° ' +
+            weather.weather[0].main
+        )
+    );
+  };
+  const fetchData = (url) => {
+    $.ajax({
+      type: 'GET',
+      url: url,
+      success: (data) => {
+        console.log(data);
+        weatherCall(data);
+      },
+      error: (error) => {
+        console.log(error);
+        $('').append('<div>').text(`An error occurred. Please try again.`);
+      },
+      complete: () => {},
+    });
+  };
+  fetchData(url);
 });
