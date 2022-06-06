c8a9474d1f14d27f48487affecb78ff7fdf51b2c
Devon Fox <foxdev@pdx.edu>
2022-06-06 09:37:32 -0700

    updated main page to contain a weather API call

--- js/app.js
+++ js/app.js
@@ -1,26 +1,16 @@
 $(document).ready(function () {
-  //Below is my API call for weather and I've commented it out, as it perhaps is not important
   const url =
     'https://api.openweathermap.org/data/2.5/weather?lat=45.5152&lon=-122.6784&appid=0a8f59036af137c0dc466abd93781d1c&units=imperial';
-  // $('#main').append('<div>').attr('id', 'weather');
-  // $("#okay").append($("<p>").text('Portland, OR Weather:'));
+
   const weatherCall = (weather) => {
     // let today = Date();
     // console.log(today)
     $('#weather').append(
-      $('<div>')
+      $('<span>')
         .css({
-          fontWeight: '100',
           color: 'black',
-          fontSize: '1.25em',
         })
-        .text(
-          weather.name +
-            ' ' +
-            parseInt(weather.main.temp) +
-            '° ' +
-            weather.weather[0].main
-        )
+        .text(parseInt(weather.main.temp) + '° ' + weather.weather[0].main)
     );
   };
   const fetchData = (url) => {
@@ -39,5 +29,4 @@ $(document).ready(function () {
     });
   };
   fetchData(url);
-  
 });
